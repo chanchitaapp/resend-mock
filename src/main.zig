7bb7b4d41574a15abdc74ae3341b644b12cc58e7
Luis Quiñones <lpaandres2020@gmail.com>
2024-09-23 15:18:43 -0600

    feat: little draft about integration with httpz

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,44 @@
 const std = @import("std");
+const httpz = @import("httpz");
 
 const resend_types = @import("resend_types.zig");
 
 pub fn main() !void {
-    const json_content = "{\"from\":\"test@example.com\",\"to\":[\"test@example.com\"],\"subject\":\"Test email\", \"cc\": 20}";
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
 
-    const allocator = std.heap.page_allocator;
+    var server = try httpz.Server().init(allocator, .{ .port = 8900 });
+    defer {
+        server.stop();
+        server.deinit();
+    }
 
-    const parse_options = std.json.ParseOptions{
-        .ignore_unknown_fields = true,
-    };
+    var router = server.router();
+    router.post("/emails", sendEmail);
 
-    const parsed = std.json.parseFromSlice(resend_types.SendEmailRequest, allocator, json_content, parse_options) catch |err| {
-        std.debug.print("Error: {any}\n", .{err});
-        return;
-    };
-
-    defer parsed.deinit();
-
-    const request = parsed.value;
-
-    std.debug.print("From: {s}, To: {s}, Subject: {s}, More... {any}\n", .{ request.from, request.to, request.subject, request.cc });
+    try server.listen();
 }
+
+fn sendEmail(_: *httpz.Request, res: *httpz.Response) !void {
+    res.status = 200;
+    try res.json(.{ .id = "5", .name = "Teg" }, .{});
+}
+
+// const json_content = "{\"from\":\"test@example.com\",\"to\":[\"test@example.com\"],\"subject\":\"Test email\", \"cc\": 20}";
+
+// const allocator = std.heap.page_allocator;
+
+// const parse_options = std.json.ParseOptions{
+//     .ignore_unknown_fields = true,
+// };
+
+// const parsed = std.json.parseFromSlice(resend_types.SendEmailRequest, allocator, json_content, parse_options) catch |err| {
+//     std.debug.print("Error: {any}\n", .{err});
+//     return;
+// };
+
+// defer parsed.deinit();
+
+// const request = parsed.value;
+
+// std.debug.print("From: {s}, To: {s}, Subject: {s}, More... {any}\n", .{ request.from, request.to, request.subject, request.cc });
